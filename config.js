184d5603d8ba7237efe1e8c58f8d7762ec9f7e4c
unknown <clayton.weller@gmail.com>
2018-07-26 17:37:56 -0600

    Test on travis and push to heroku... hopefully

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/blogdb';
+// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/blogdb';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/blogdb-test';
 exports.PORT = process.env.PORT || 8080
 
-// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://clayton:1two34five!@ds117848.mlab.com:17848/blogdb';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://clayton:1two34five!@ds117848.mlab.com:17848/blogdb';
 // exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://clayton:1two34five!@ds117848.mlab.com:17848/blogdb';
 // exports.PORT = process.env.PORT || 8080
